5700d6c6f36f48fb211821cd8db9792b23928559
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:19:24 +0200

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Activity2a.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Message()
+        {
+            return View("Message");
+        }
+        public IActionResult Welcome(string name,int secretNumber=13)
+        {
+            ViewBag.UserName = name;
+            ViewBag.Secret=secretNumber;
+            return View();
+        }
+    }
+}
